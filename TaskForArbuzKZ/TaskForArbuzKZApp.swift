5c90ff70b916b0e3af8144bdec732db5f4f811cd
Merey <mereybulatova05@gmail.com>
2024-05-19 20:16:44 +0500

    Initial Commit

---
+++ TaskForArbuzKZ/TaskForArbuzKZApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskForArbuzKZApp.swift
+//  TaskForArbuzKZ
+//
+//  Created by Мерей Булатова on 19.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskForArbuzKZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
